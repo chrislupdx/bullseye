65078278bcc5787c2fd96cea5f7ec452c7d0656e
chrislupdx <christopherlu.pdx@gmail.com>
2020-03-29 12:57:07 -0700

    got button to hello world

---
+++ slider-guesser/slider-guesser/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  slider-guesser
+//
+//  Created by Cascadia Carbon on 3/29/20.
+//  Copyright © 2020 R'lyeh Holdings. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func showAlert() {
+        print("heyyo")
+        
+    }
+
+}
+
