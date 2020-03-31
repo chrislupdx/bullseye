6419c080e88c5fb8f291eba9d953a31464c9ac33
chrislupdx <christopherlu.pdx@gmail.com>
2020-03-30 21:18:56 -0700

    that 's how to pass vals from ui to a var and print them

--- slider-guesser/slider-guesser/ViewController.swift
+++ slider-guesser/slider-guesser/ViewController.swift
@@ -9,16 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var currentValue: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "alerttitle", message: "Alert body text", preferredStyle: .alert)
+        let message = "The value of slider is \(currentValue)"
+        let alert = UIAlertController(title: "alerttitle", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "buttontext!?", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
+ 
+    @IBAction func sliterMOved(_ slider: UISlider){
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+    }
+        
 }
 
